ae92cff709243bf9e9d56cbb16304f2bd152d2a4
Ben Schwartz <schwartz.ben0@gmail.com>
2024-08-24 14:15:24 -0300

    Add createGroup function to hook

--- web/hooks/useGroups.tsx
+++ web/hooks/useGroups.tsx
@@ -9,6 +9,10 @@ export const useGroups = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
+  useEffect(() => {
+    refetchGroups();
+  }, []);
+
   const refetchGroups = async () => {
     try {
       setLoading(true);
@@ -31,9 +35,25 @@ export const useGroups = () => {
     }
   };
 
-  useEffect(() => {
-    refetchGroups();
-  }, []);
+  const createGroup = async (group: IGroup) => {
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/group', group);
 
-  return { groups, loading, error, refetchGroups };
+      if (response.status !== 201) {
+        setError('Failed to create group');
+        return;
+      }
+
+      console.log('created group', response.data.group);
+      setGroups([...groups, response.data.group]);
+      setError(null);
+    } catch (err) {
+      setError('Failed to create group');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { groups, loading, error, refetchGroups, createGroup };
 };
