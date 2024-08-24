5d5377ca4ba256301acba73c6baf8d2c460e560f
Alejandro Alvarez <a.alejandro.alvarez.a@gmail.com>
2024-08-24 12:37:03 -0300

    feat(wallet): add button wallet

--- web/components/Global/NavbarLayout.tsx
+++ web/components/Global/NavbarLayout.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { WalletButton } from '../solana/solana-provider';
+import { ClusterUiSelect } from '../cluster/cluster-ui';
 
 const NavbarLayout = () => {
-  return <div></div>;
+  return (
+    <div>
+      <WalletButton />
+      <ClusterUiSelect />
+    </div>
+  );
 };
 
 export default NavbarLayout;
