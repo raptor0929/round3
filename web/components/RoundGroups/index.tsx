ea42edcab5333bbd719716f6504fe281e9aa782a
Ben Schwartz <schwartz.ben0@gmail.com>
2024-08-25 01:28:09 -0300

    Only show "Your Rounds" if wallet is connected

--- web/components/RoundGroups/index.tsx
+++ web/components/RoundGroups/index.tsx
@@ -4,9 +4,12 @@ import React from 'react';
 import CardRound from './CardRound';
 import { Modal, ModalContent, Spinner } from '@nextui-org/react';
 import { useGroups } from '@/hooks/useGroups';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletButton } from '../solana/solana-provider';
 
 const RoundGroups = () => {
   const { myGroups, loading } = useGroups();
+  const { connected } = useWallet();
 
   return (
     <div className="h-full w-full relative ">
@@ -20,11 +23,19 @@ const RoundGroups = () => {
         <h2 className="text-7xl h-1/5 font-bold font-sans flex items-center">
           Your Rounds
         </h2>
-        <div className="flex gap-4 flex-wrap">
-          {myGroups.map((group) => (
-            <CardRound key={group.id} group={group} />
-          ))}
-        </div>
+        {connected ? (
+          <div className="flex gap-4 flex-wrap">
+            {myGroups.map((group) => (
+              <CardRound key={group.id} group={group} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex justify-center items-center">
+            <WalletButton className="w-full bg-primaryOutline text-white py-6">
+              Connect Wallet
+            </WalletButton>
+          </div>
+        )}
       </div>
       {loading && (
         <Modal isOpen={loading} hideCloseButton>
